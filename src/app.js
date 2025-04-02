28f57c1142aa9b6dbc8a4d98071bf1efc0840c97
Pushpendra <pushpendra616singh@gmail.com>
2025-04-03 00:21:35 +0530

    exploring get,post,delete,patch

--- src/app.js
+++ src/app.js
@@ -2,12 +2,30 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-  res.send("Hello Hello Heloo");
+// app.use("/hello", (req, res) => {
+//   res.send("Hello Hello Heloo");
+// });
+
+// // it wont work because of the previous middleware
+// // app.use("/hello/2", (req, res) => {
+// //   res.send("hi hi hi hi ");
+// // });
+
+// app.use("/bye", (req, res) => {
+//   res.send("bye");
+// });
+
+app.get("/user", (req, res) => {
+  res.send({ firstnname: "John", lastname: "Doe" });
 });
 
-app.use("/bye", (req, res) => {
-  res.send("bye");
+app.post("/user", (req, res) => {
+  console.log("Save Data to the database");
+  res.send("Data successfully saved to the database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted user");
 });
 
 app.use("/test", (req, res) => {
