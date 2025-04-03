ff1255bc961340f2ea0ad32a242cfb065972d9d5
Pushpendra <pushpendra616singh@gmail.com>
2025-04-03 14:01:39 +0530

    multiple route handler chapter 5

--- src/app.js
+++ src/app.js
@@ -2,40 +2,71 @@ const express = require("express");
 
 const app = express();
 
-// it works with ac or abc both becous b is optional
-app.get("/ab?c", (req, res) => {
-  res.send({ firstnname: "John", lastname: "Doe" });
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    // first route handlerr
+    console.log("Handling the route User !!");
+    res.send("Hello from user route1");
+    next(); // call the next middleware or route handler
+    // now this will not be executed because we have already sent a response,it will goes to next route handler but give error, it will throw an error,because we are trying to send a response again in the next middleware, becouse Express does not allow sending multiple responses for a single request, leading to this error:
+  },
+  // second route handler
+  (req, res) => {
+    console.log("Handling the route user 2nd !!");
+    res.send("Hello from user route2");
+  }
+);
 
-// it works with abc,ab, abbb, abbbbbb but not with ac
-app.get("/ab+c", (req, res) => {
-  res.send({ firstnname: "John", lastname: "Doe" });
-});
+// Control Returns to the First Middleware (after next()):
 
-// it means it should start with ab and end with cd and in between there can be anything eg abcd, abcd, abbbcd, abbbbbbcd
-app.get("/ab*cd", (req, res) => {
-  res.send({ firstnname: "John", lastname: "Doe" });
-});
+// JavaScript event loop resumes execution from the next line.
 
-// it means bc is optional eg abcd, abcd,ad, abbbcd, abbbbbbcd
-app.get("/a(bc)?d", (req, res) => {
-  res.send({ firstnname: "John", lastname: "Doe" });
-});
+// res.send("Hello from user route11") tries to execute.
 
-//
-app.get("/a(bc)+d", (req, res) => {
-  res.send({ firstnname: "John", lastname: "Doe" });
-});
+// BUT RESPONSE IS ALREADY SENT! → ❌ Error: "Cannot set headers after they are sent to the client".
+app.use(
+  "/user1",
+  (req, res, next) => {
+    // first route handlerr
+    console.log("Handling the route User !!");
 
-// 'a' letter should be there and after that it can be anything but not 'b' letter, example correct - ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al, am, an, ao, ap, aq, ar, as, at, au, av, aw, ax, ay, az,its all correct becouse 'a' is there
-app.get("/a/", (req, res) => {
-  res.send({ firstnname: "John", lastname: "Doe" });
-});
+    next(); // call the next middleware or route handler
+    res.send("Hello from user route11");
+  },
+  // second route handler
+  (req, res) => {
+    console.log("Handling the route user 2nd !!");
+    res.send("Hello from user route22");
+  }
+);
 
-// it (.*)means it should start with anything but should end with fly only
-app.get("/.*fly$/", (req, res) => {
-  res.send({ firstnname: "John", lastname: "Doe" });
-});
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the router user 1st !!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the router user 2nd !!");
+    //res.send("Hello from user route2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the router user 3rd !!");
+    // res.send("Hello from user route3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the router user 4th !!");
+    //res.send("Hello from user route4");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the router user 5th !!");
+    res.send("Hello from user route5");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
